aacd50248de790525963f48dea1a77ad20e426b9
renercrows <ungredda95@gmail.com>
2022-08-15 16:22:49 -0400

    Add Todo List App

---
+++ src/components/TodoItem.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import styles from './TodoItem.module.css';
+
+class TodoItem extends React.Component {
+  state = {
+    editing: false,
+  };
+
+  handleEditing = () => {
+    this.setState({
+      editing: true,
+    });
+  };
+
+  handleUpdatedDone = (event) => {
+    if (event.key === 'Enter') {
+      this.setState({ editing: false });
+    }
+  }
+
+  render() {
+    const { completed, id, title } = this.props.todo;
+    const completedStyle = {
+      fontStyle: 'italic',
+      color: '#595959',
+      opacity: 0.4,
+      textDecoration: 'line-through',
+    };
+    const viewMode = {};
+    const editMode = {};
+
+    if (this.state.editing) {
+      viewMode.display = 'none';
+    } else {
+      editMode.display = 'none';
+    }
+    return (
+      <li className={styles.item}>
+
+        <input
+          type="checkbox"
+          className={styles.checkbox}
+          checked={completed}
+          onChange={() => this.props.handleChangeProps(id)}
+        />
+
+        <button onClick={() => this.props.deleteTodoProps(id)}>Delete</button>
+
+        <input
+          type="text"
+          style={editMode}
+          className={styles.textInput}
+          value={title}
+          onChange={(e) => {
+            this.props.setUpdate(e.target.value, id);
+          }}
+          onKeyDown={this.handleUpdatedDone}
+        />
+        <span style={completed ? completedStyle : null}>{title}</span>
+        <div onDoubleClick={this.handleEditing} style={viewMode}>
+          ...
+        </div>
+      </li>
+    );
+  }
+}
+
+export default TodoItem;
